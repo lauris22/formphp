be28ec54aa3dca52cb13634d2f7cef2acf367359
lauris22 <llauraalejandra11@gmail.com>
2024-08-06 15:12:09 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion= $_POST ['identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['Cargo];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleados son:  ".NombreApellidos;
+echo "El cargo del empleaado es: ".Cargo;
+echo "el salario del empleado es:  ".salario;
+?>
