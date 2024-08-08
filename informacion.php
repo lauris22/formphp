8cfe4d320410e464484271c23a21725437d8fa94
lauris22 <llauraalejandra11@gmail.com>
2024-08-08 14:24:35 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,5 +1,5 @@
 <?php
-$Identificacion=$_POST ['identificacion'];
+$Identificacion=$_POST ['Identificacion'];
 $NomApellidos=$_POST ['NomApellidos'];
 $Cargo=$_POST ['Cargo];
 $Salario=$_POST ['Salario'];
